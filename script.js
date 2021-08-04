71fd2aab1f30e44365a96b0e1b2a561f5997ad97
kennytrinh <kenny.trinh4444@gmail.com>
2021-08-04 15:50:07 -0500

    firebase

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("testing")
+import './style.css'
+
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBayxDYyjcglnz61-YHgdz0h1YVkrL4cBI",
+  authDomain: "karaoke-b9361.firebaseapp.com",
+  projectId: "karaoke-b9361",
+  storageBucket: "karaoke-b9361.appspot.com",
+  messagingSenderId: "152169262096",
+  appId: "1:152169262096:web:0f944cd70bfd4f6efdee95",
+  measurementId: "G-FP6G8017CV"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+}
+
+const firestore = firebase.firestore()
+
+
